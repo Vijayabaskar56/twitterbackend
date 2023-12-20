13f827e847da6ac09a943291cb09ab272bbdcfb7
Vijayabaskar56 <vj2k02@gmail.com>
2023-12-20 16:50:52 +0530

    feat: est-deploy

--- controllers/healthcheck.js
+++ controllers/healthcheck.js
@@ -2,11 +2,9 @@ const healthcheck = (db) => {
   return async (req, res) => {
     try {
       await db.sequelize.authenticate();
-      await db.sequelize.close();
       res.status(200).json({ status: "Sucessfully connected" });
     } catch (error) {
       console.log(error);
-      await db.sequelize.close();
       res.status(500).json({ status: "failed to connect" });
     }
   };
